b6227458e2b0dcf0bbf65ea45fba37f8247391b4
Aman Mahajan <ammahajan@expedia.com>
2018-06-02 01:25:47 +0530

    formatting changes

--- src/PC_Remote_Controller/Main.java
+++ src/PC_Remote_Controller/Main.java
@@ -4,10 +4,9 @@ package PC_Remote_Controller;
  * @author Aman Mahajan
  */
 public class Main {
-
     public static void main(String[] args) {
     String url="https://api.telegram.org/bot";
-    String tokenID="enter_token_id";    
-    new amanmjBot(url,tokenID).start();      
+    String tokenID="enter_token_id";
+    new amanmjBot(url,tokenID).start();
     }
 }
